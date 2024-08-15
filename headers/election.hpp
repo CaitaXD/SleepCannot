5ae53e5bff30fbe44943f1fd2385b845327618ba
hugobbi <hgugobbi@gmail.com>
2024-08-15 18:05:45 -0300

    Separating node and election

---
+++ headers/election.hpp
@@ -0,0 +1,132 @@
+#ifndef ELECTION_H_
+#define ELECTION_H_
+
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <string>
+#include "management.hpp"
+#include "Net/Socket.hpp"
+#include "macros.h"
+#include "node.hpp"
+
+#define TIMEOUT_ELECTION 2500 // ms
+#define TIMEOUT_COORDINATOR 5000 // ms
+
+/*
+    Assumptions:
+    process fails by stopping and returns from failure by restarting.
+    there is a failure detector which detects failed processes.
+    message delivery between processes is reliable.
+    each process knows its own process id and address, and that of every other process.
+    each process has a unique priority number.
+    all processes in the system are fully connected.
+    
+    Goals:
+    the process with the highest priority number will be elected as coordinator.
+    there should be only one leader among the processes.
+    all Processes agree on who is the leader.
+
+    Election message format:
+    E<op><id>
+    E: election message
+    op: operation (e for election, a for answer, c for coordinator)
+    id: node id
+*/
+
+namespace Election {
+    void run_election(Node& node); // starts election process
+    void send_election(Node& node); // sends election message to all participants with higher id
+    void answer_election(Node& node); // answers election message
+    void send_coordinator(Node& node); // sends coordinator message to all participants
+    // TODO: implement on monitoring maybe
+    bool should_run_election(); // determined in monitoring service
+    bool check_reply_from_election(Node& node); // check if there is a reply from election message
+    int check_coordinator(Node& node); // check if there is a coordinator message and returns its id (or -1 if no coordinator)
+} 
+
+#endif ELECTION_H_
+
+#ifndef ELECTION_IMPLEMENTATION
+#define ELECTION_IMPLEMENTATION
+
+// Send coordinator message to all participants
+void Election::send_coordinator(Node& node) {
+    node.participants.lock();
+    for (auto &[host, participant] : node.participants.map) {
+        participant.socket->send("Ec" + std::to_string(node.info.id));
+    }
+    node.participants.unlock();
+}
+
+// Send election message to participants with higher id
+void Election::send_election(Node& node) {
+    if (node.has_started_election) return; // only one election message per node
+    node.has_started_election = true;
+    node.participants.lock();
+    for (auto &[host, participant] : node.participants.map) {
+        if (node.info.id < participant.id) {
+            participant.socket->send("Ee" + std::to_string(node.info.id));
+        }
+    }
+    node.participants.unlock();
+}
+
+// run in thread (maybe on listen, which might be on monitoring service)
+// Answers election message of lower id node and starts election if it has not started
+void Election::answer_election(Node& node, int sender_id) {
+    if (node.info.id > sender_id) {
+        node.participants.lock();
+        for (auto &[host, participant] : node.participants.map) {
+            if (participant.id != sender_id) continue;
+            participant.socket->send("Ea" + std::to_string(node.info.id));
+            if (!node.has_started_election) Election::run_election(node);
+            node.participants.unlock();
+            return;
+        }
+        node.participants.unlock();
+    }
+}
+
+// run in thread
+// Starts election process
+void Election::run_election(Node& node) {
+    // Sends coordinator message if it has the highest id
+    bool highest_id = true;
+    node.participants.lock();
+    for (auto &[host, participant] : node.participants.map) {
+        if (!participant.status) continue;
+        if (node.info.id < participant.id) {
+            highest_id = false;
+            break;
+        }
+    }
+    node.participants.unlock();
+    if (highest_id) {
+        Election::send_coordinator(node);
+        node.has_started_election = false;
+        return;
+    }
+    // Else, send election message to all participants with higher id
+    Election::send_election(node);
+    // Wait for answers
+    msleep(TIMEOUT_ELECTION);
+    if (Election::check_reply_from_election(node)) {
+        msleep(TIMEOUT_COORDINATOR); // waits for coordinator message, if timeout, starts election
+        int coordinator_id = Election::check_coordinator(node);
+        if (coordinator_id == -1) {
+            node.has_started_election = false;
+            Election::run_election(node);
+        }
+        else {
+            node.has_started_election = false;
+            node.manager_id = coordinator_id; // updates new coordinator
+            return;
+        }
+    }
+    // If no answer, send coordinator message
+    Election::send_coordinator(node);
+    node.has_started_election = false;
+}
+
+#endif ELECTION_IMPLEMENTATION
--- headers/node.hpp
+++ headers/node.hpp
@@ -15,23 +15,6 @@
 #include "Net/Socket.hpp"
 #include "macros.h"
 
-#define TIMEOUT_ELECTION 2500 // ms
-#define TIMEOUT_COORDINATOR 5000 // ms
-
-/*
-    Assumptions:
-    process fails by stopping and returns from failure by restarting.
-    there is a failure detector which detects failed processes.
-    message delivery between processes is reliable.
-    each process knows its own process id and address, and that of every other process.
-    each process has a unique priority number.
-    all processes in the system are fully connected.
-    
-    Goals:
-    the process with the highest priority number will be elected as coordinator.
-    there should be only one leader among the processes.
-    all Processes agree on who is the leader.
-*/
 
 struct Message {
     int id;
@@ -39,27 +22,19 @@ struct Message {
 };
 
 class Node {
-    participant_t info; // info about this node
-    ParticipantTable participants; // every node keeps a copy of the participants table
-    int manager_id = -1; // id of the manager node
-    bool has_started_election = false;
-    std::mutex msg_mutex; // only one thread can access messages at a time
-    std::vector<Message> messages; // messages received by this node
+    public:
+        participant_t info; // info about this node
+        ParticipantTable participants; // every node keeps a copy of the participants table
+        int manager_id = -1; // id of the manager node
+        bool has_started_election = false;
+        std::mutex msg_mutex; // only one thread can access messages at a time
+        std::vector<Message> messages; // messages received by this node
 
-    Node(int id, int manager_id);
-    ~Node();
-    void start_node(); // this function should connect node to all other nodes
-    void end_node();
-
-    bool should_run_election(); // determined in monitoring service
-    void run_election();
-    void send_election();
-    void answer_election();
-    void send_coordinator();
-    
-    void listen(); // listen for messages, updating message vector
-    bool check_reply_from_election(); // check if there is a reply from election message
-    int check_coordinator(); // check if there is a coordinator message and returns its id (or -1 if no coordinator)
+        Node(int id, int manager_id);
+        ~Node();
+        void start_node(); // this function should connect node to all other nodes
+        void end_node();
+        void listen(); // listen for messages, updating message vector  
 };
 
 #endif // NODE_H_
@@ -67,154 +42,37 @@ class Node {
 #ifndef NODE_IMPLEMENTATION
 #define NODE_IMPLEMENTATION
 
-// run in thread
+Node::Node(int id, int manager_id) {
+
+}
+
+Node::~Node() {
+    
+}
+
+void Node::start_node() {
+
+}
+
+void Node::end_node() {
+
+}
+
+// run in thread (TODO: implement on monitoring maybe)
 void Node::listen() {
     this->msg_mutex.lock();
     this->messages.clear();
     this->msg_mutex.unlock();
     while (true) {
         std::string msg;
-        this->info.socket->recv(&msg);
-        char msg_type = msg[0];
+        this->info.socket->recv(&msg); // check if it is blocking
+        char msg_type = msg[0]; // check if message is of type 'e', 'c' or 'a'
         int id = std::stoi(msg.substr(2));
         this->msg_mutex.lock();
         this->messages.push_back(Message{.id = id, .msg = msg_type});
         this->msg_mutex.unlock();
         msleep(300); // let other threads get the GODDAMN mutex
     }
-}
-
-void Node::send_coordinator() {
-    this->participants.lock();
-    // send coordinator message to all participants
-    for (auto &[host, participant] : participants.map) {
-        participant.socket->send("c," + std::to_string(this->info.id));
-    }
-    this->participants.unlock();
-}
-
-void Node::send_election() {
-    if (this->has_started_election) return; // only one election message per node TODO: no busy waiting
-    this->has_started_election = true;
-    // send election message to participants with higher id
-    this->participants.lock();
-    for (auto &[host, participant] : participants.map) {
-        if (this->info.id < participant.is_manager) {
-            participant.socket->send("e," + std::to_string(this->info.id));
-        }
-    }
-    this->participants.unlock();
-}
-
-// run in thread (maybe on listen)
-void Node::answer_election() {
-    this->msg_mutex.lock();
-    if (this->messages.size() == 0) {
-        this->msg_mutex.unlock();
-        return;
-    }
-    for (auto it = this->messages.begin(); it != this->messages.end(); ++it) {
-        auto &msg = *it;
-        if (msg.msg == 'e') {
-            int dest_id = msg.id;
-            if (this->info.id < dest_id) {
-                this->participants.lock();
-                for (auto &[host, participant] : participants.map) {
-                    if (participant.id == dest_id) {
-                        participant.socket->send("a," + std::to_string(this->info.id));
-                        if (!this->has_started_election) this->run_election();
-                        this->messages.erase(it);
-                        this->participants.unlock();
-                        this->participants.unlock();
-                        return;
-                    }
-                }
-                this->participants.unlock();
-            }
-        }
-    }
-    this->participants.unlock();
-}
-
-// run in thread (maybe on listen)
-int Node::check_coordinator() {
-    this->msg_mutex.lock();
-    if (this->messages.size() == 0) {
-        this->msg_mutex.unlock();
-        return false;
-    }
-    for (auto it = this->messages.begin(); it != this->messages.end(); ++it) {
-        auto &msg = *it;
-        if (msg.msg == 'c') {
-            this->msg_mutex.unlock();
-            this->messages.erase(it);
-            return msg.id;
-        }
-    }
-    this->msg_mutex.unlock();
-    return -1;
-}
-
-bool Node::check_reply_from_election() {
-    this->msg_mutex.lock();
-    if (this->messages.size() == 0) {
-        this->msg_mutex.unlock();
-        return false;
-    }
-    for (auto it = this->messages.begin(); it != this->messages.end(); ++it) {
-        auto &msg = *it;
-        if (msg.msg == 'a') {
-            if (this->info.id < msg.id) {
-                this->msg_mutex.unlock();
-                this->messages.erase(it);
-                return true;
-            }
-        }
-    }
-    this->msg_mutex.unlock();
-    return false;
-}
-
-// run in thread
-void Node::run_election() {
-    // if (!this->should_run_election()) return;
-    // sends coordinator message if it has the highest id
-    bool highest_id = true;
-    this->participants.lock();
-    for (auto &[host, participant] : participants.map) {
-        if (!participant.status) continue;
-        if (this->info.id < participant.id) {
-            highest_id = false;
-            break;
-        }
-    }
-    this->participants.unlock();
-    if (highest_id) {
-        this->send_coordinator();
-        this->has_started_election = false;
-        return;
-    }
-    // else, send election message to all participants with higher id
-    this->send_election();
-    // wait for answers
-    msleep(TIMEOUT_ELECTION);
-    if (this->check_reply_from_election()) {
-        msleep(TIMEOUT_COORDINATOR); // waits for coordinator message, if timeout, starts election
-        int coordinator_id = this->check_coordinator();
-        if (coordinator_id == -1) {
-            this->has_started_election = false;
-            this->run_election();
-        }
-        else {
-            this->has_started_election = false;
-            this->manager_id = coordinator_id;
-            return;
-        }
-    }
-    // if no answer, send coordinator message
-    this->send_coordinator();
-    this->has_started_election = false;
-    return;
 }
 
 #endif // NODE_IMPLEMENTATION
